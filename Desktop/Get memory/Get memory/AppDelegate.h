650c405f0c7a3acca38d45e2eb282369b5a7b41c
lidongjie <dongjie@weico.com>
2018-11-12 09:36:34 +0800

    检测设备内存

---
+++ Desktop/Get memory/Get memory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Get memory
+//
+//  Created by Geraint on 2018/11/10.
+//  Copyright © 2018 kilolumen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/Get memory/Get memory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Get memory
+//
+//  Created by Geraint on 2018/11/10.
+//  Copyright © 2018 kilolumen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
